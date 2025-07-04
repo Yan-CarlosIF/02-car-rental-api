ce9a3438e511d7f482dbf5d799cdf97141c25895
Yan Carlos <yan312005@gmail.com>
2025-07-04 01:26:39 -0300

    docs: app documentation with swagger

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { router } from "./routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
@@ -7,6 +9,11 @@ app.use(express.json());
 
 app.use(router);
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.listen(3333, () => {
-  console.log("Server is running at http://localhost:3333");
+  console.log(
+    `Server is running at http://localhost:3333
+docs at http://localhost:3333/api-docs`
+  );
 });
